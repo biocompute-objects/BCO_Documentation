0aae6a81b096af8f2551bc8b5df9fc965a8d283a
HadleyKing <hadley_king@gwu.edu>
2020-01-19 09:09:45 -0500

    Remove IEEE_Docs
    Remove IEEE_Docs and update to scripts/log.sh

--- scripts/log.sh
+++ scripts/log.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -l
+
 git log master..dev --pretty=format:"{  'commit': '%H', 'author': '%aN',  'email': '%aE',  'date': '%ad',  'subject': '%f',  'message': '%b'},"     $@ | sed -e "s/\"//g"| sed -e "s/'/\"/g" | tr '\n\r' ' '|     perl -pe 'BEGIN{print "["}; END{print "]"}' |     perl -pe 's/}, ]/}]/' > ./scripts/git-log.json
 
 git log master..dev --numstat     --format='%H' $@ | perl -lawne ' if (defined $F[1]) {            print qq#{"insertions": "$F[0]", "deletions": "$F[1]", "path":"$F[2]"},#        } elsif (defined $F[0]) {            print qq#],\n"$F[0]": [#        };        END{print qq#],#}' | tail -n +2 | perl -wpe 'BEGIN{print "{"}; END{print "}"}' | tr '\n\r' ' ' | perl -wpe 's#(]|}),\s*(]|})#$1$2#g' | perl -wpe 's#,\s*?}$#}#' > ./scripts/git-stat.json
